ad61d08ddb95b235d98f11c7f181345cb4094dca
kirillAnoshko <112968612+kirillAnoshko@users.noreply.github.com>
2023-06-26 17:56:14 +0300

    Add files via upload

--- analyze_text/main.py
+++ analyze_text/main.py
@@ -3,9 +3,10 @@ import pymorphy3
 from typing import NoReturn
 
 class TextAnalyze:
-    def __init__(self, file_name=None):
+    def __init__(self, file_name=None, part_of_speech=["VERB", "NOUN"]):
         if file_name is None:
             raise Exception("Не указан файл для анализа!")
+        self.part_of_speech = part_of_speech
         self.read_file(file_name)
         self.check_empty_text()
         self.prepare_text()
@@ -30,12 +31,12 @@ class TextAnalyze:
         self.text = self.text.lower()
         self.clean_text = ''.join(re.findall(r'[\w\s-]', self.text))
 
-    def make_analyzed_words(self, part_of_speech=["VERB", "NOUN"]) -> list:
+    def make_analyzed_words(self) -> list:
         self.analyzed_words = []
         morph = pymorphy3.MorphAnalyzer()
         for word in self.words:
             parsed_word = morph.parse(word)[0]
-            if parsed_word.tag.POS in part_of_speech:
+            if parsed_word.tag.POS in self.part_of_speech:
                 self.analyzed_words.append(parsed_word.normal_form)
                     
     def print_text(self):
